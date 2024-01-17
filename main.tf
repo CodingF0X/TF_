daa74879d4c68cbb7af90d7998fbaf6175bbeae0
CodingF0X <roody9a@gmail.com>
2024-01-17 01:34:29 +0100

    Added: ASG, Lifecycle and data_source

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ provider "aws" {
   shared_credentials_files = ["C:/Users/Jeffar/.aws/credentials"]
 }
 
-resource "aws_instance" "EC2_first_Instance" {
-  ami                    = "ami-0fe8bec493a81c7da"
-  vpc_security_group_ids = [aws_security_group.instance.id] // referencing to the security group we are using
+resource "aws_launch_configuration" "EC2_Instance" {
+  image_id        = "ami-0fe8bec493a81c7da"
+  security_groups = [aws_security_group.instance.id] // referencing to the security group we are using
 
   instance_type = "t3.micro"
   user_data     = <<-EOF
@@ -25,13 +25,29 @@ resource "aws_instance" "EC2_first_Instance" {
               nohup busybox httpd -f -p ${var.port_number} &
               EOF
 
-  user_data_replace_on_change = true
-  tags = {
-    Name = "My-AWS-Instance"
+  //-- Lifecycle --//
+  # Required when using a launch configuration with an ASG as it does the following:
+  // 1-it creates a new instance
+  // 2-update any references that were pointing at the old resource to point to the replacement 
+  // 3- deletes the old instance
+  lifecycle {
+    create_before_destroy = true
   }
-
 }
 
+resource "aws_autoscaling_group" "ASG_example" {
+  launch_configuration = aws_launch_configuration.EC2_Instance.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
 
 resource "aws_security_group" "instance" { //To allow the EC2 Instance to receive traffic on port 8080, we need to create security group
   name = "terraform-example-instance"
@@ -49,8 +65,19 @@ variable "port_number" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.EC2_first_Instance.public_ip
-  description = "The public IP address of the web server"
+# output "public_ip" {
+#   value       = aws_instance.EC2_first_Instance.public_ip
+#   description = "The public IP address of the web server"
 
+# }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
